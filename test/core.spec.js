26af0a27fcc343b6ef73129c5cc66a41235fe38a
Daniel Kobina <danielkobina@Daniels-MacBook-Pro.local>
2016-05-08 11:19:29 -0400

    update initialize test to insure it return false if worker is not supported

--- test/core.spec.js
+++ test/core.spec.js
@@ -1,10 +1,17 @@
 import {expect} from 'chai';
 import sinon from 'sinon';
+import _ from 'lodash-fp';
 import Core from '../src/core/index';
 
 describe('Core', () => {
-  it('should return true when worker feature is supported', () => {
-    expect(Core.initialize()).to.be.equal(true);
+  describe('initialize()', () => {
+    it('should return true when worker feature is supported', () => {
+      expect(Core.initialize()).to.be.equal(true);
+    });
+    it('should return false when worker feature is not supported', () => {
+      sinon.stub(_, "isFunction", () => false);
+      expect(Core.initialize()).to.be.equal(false);
+    });
   });
 
   // timeout failure is expected if the function don't work properly
