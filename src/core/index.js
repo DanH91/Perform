2a4651e8e0eddab3df99b2fa04d199917589042a
Daniel Kobina <danielkobina@Daniels-MacBook-Pro.local>
2016-05-08 12:40:44 -0400

    refactor core spawn funtion using rxjs observable

--- src/core/index.js
+++ src/core/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash-fp';
+import Rx from 'rxjs';
 
 export default (() => {
   return {
@@ -8,23 +9,41 @@ export default (() => {
 })();
 
 /**
+ * Check worker support
+ * @return {Bool} suppport
+ */
+function initialize() {
+  return _.isFunction(window.Worker);
+}
+
+/**
   * Spawn a dedicated worker.
   * @param  {string} script - worker script.
   * @return {object} worker wrapper  -  worker wrapper object.
   */
 function spawn(script) {
-  let w = new Worker(script);
-  return {
-    subscribe(callback) {
-      w.onmessage = callback;
-    }
+  let w = new window.Worker(script);
+  let observer = new Rx.Subject();
+  w.onmessage = _Callback;
+  let ctx = {
+    map
   };
-}
+  return ctx;
 
-/**
- * Check worker support
- * @return {Bool} suppport
- */
-function initialize() {
-  return _.isFunction(Worker);
+  /**
+   * Map over stream of worker event data.
+   * @param  {function} callback .
+   * @return {object} token - subscription token.
+   */
+  function map(callback) {
+    return observer.subscribe(callback);
+  }
+
+  /**
+   * Worker message event callback.
+   * @param  {object} event .
+   */
+  function _Callback(event) {
+    observer.next(event.data);
+  }
 }
