b9cb492b4674a081e1632e34d8c7b609ddb2fe4c
Daniel Kobina <danielkobina@Daniels-MacBook-Pro.local>
2016-05-10 06:13:40 -0400

    extract spawn function in it's own file

--- src/core/index.js
+++ src/core/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash-fp';
-import Rx from 'rxjs';
+
+import spawn from './spawn';
 
 export default (() => {
   return {
@@ -14,56 +15,4 @@ export default (() => {
  */
 function initialize() {
   return _.isFunction(window.Worker);
-}
-
-/**
-  * Spawn a dedicated worker.
-  * @param  {string} script - worker script.
-  * @return {object} worker wrapper  -  worker wrapper object.
-  */
-function spawn(script) {
-  let w = new window.Worker(script);
-  let observer = new Rx.ReplaySubject(1);
-  w.onmessage = _Next;
-  w.onerror = _Error;
-  let ctx = {
-    subscribe,
-    stop
-  };
-  return ctx;
-
-  /**
-   * Terminate worker and complete observable.
-   */
-  function stop() {
-    w.terminate();
-    observer.complete();
-  }
-
-  /**
-   * Map over stream of worker event data.
-   * @param  {function} onNext - on next value callback.
-   * @param  {function} onError - on error value callback.
-   * @param  {function} onComplete - on complete value callback.
-   * @return {object} token - subscription token.
-   */
-  function subscribe(onNext, onError, onComplete) {
-    return observer.subscribe(onNext, onError, onComplete);
-  }
-
-  /**
-   * Worker message event callback.
-   * @param  {object} event .
-   */
-  function _Next(event) {
-    observer.next(event.data);
-  }
-
-  /**
-   * Worker error event callback.
-   * @param {object} event .
-   */
-  function _Error(event) {
-    observer.error(new Error(event.message));
-  }
 }
