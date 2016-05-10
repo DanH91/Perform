56552a6117358176e9a3e17cf876caec9008943a
Daniel Kobina <danielkobina@Daniels-MacBook-Pro.local>
2016-05-10 14:56:34 -0400

    decouple worker script creation from ObjectURL creation

--- src/core/create.js
+++ src/core/create.js
@@ -3,21 +3,30 @@ import {isFunction} from 'lodash-fp';
 /**
   * Spawn a dedicated worker.
   * @param  {(string | function)} script - worker script.
-  * @return {object} worker  - spawned worker.
+  * @return {object} worker - spawned worker.
   */
 export default function create(script) {
   return new window.Worker(
-    isFunction(script) ? createTransferable(script) : script
+    isFunction(script) ? createScript(script) : script
   );
 }
 
 /**
-  * Create ObjectURL from script Blob.
+  * Create Tranferable script.
   * @param  {string} script - worker script.
+  * @return {string} objectURL - object URL of script Blob.
+  */
+export function createScript(script) {
+  return createTransferable(
+    script.toString().match(/function[^{]+\{([\s\S]*)\}$/)[1]
+  );
+}
+
+/**
+  * Create ObjectURL from Blob.
+  * @param  {string} text - worker script.
   * @return {string} objectURL  - object URL of script Blob.
   */
-export function createTransferable(script) {
-  return window.URL.createObjectURL(new window.Blob([
-    `${script.toString().match(/function[^{]+\{([\s\S]*)\}$/)[1]}`
-  ]));
+export function createTransferable(text) {
+  return window.URL.createObjectURL(new window.Blob([`${text}`]));
 }
