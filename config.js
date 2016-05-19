fb949639ffb4ab95652109e397779a73e77c9bc1
Daniel Kobina <danielkobina@Daniels-MacBook-Pro.local>
2016-05-18 21:48:12 -0400

    remove big dependencies and replace them by their lightweight versions

--- config.js
+++ config.js
@@ -859,11 +859,13 @@ System.config({
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "chai": "npm:chai@3.5.0",
     "core-js": "npm:core-js@1.2.6",
-    "lodash-fp": "npm:lodash-fp@0.10.4",
-    "rxjs": "npm:rxjs@5.0.0-beta.7",
+    "lodash.isfunction": "npm:lodash.isfunction@3.0.8",
+    "lodash.isstring": "npm:lodash.isstring@4.0.1",
+    "lodash.last": "npm:lodash.last@3.0.0",
+    "rx-lite": "npm:rx-lite@4.0.8",
     "sinon": "npm:sinon@1.17.4",
     "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.3.0"
+      "assert": "npm:assert@1.4.0"
     },
     "github:jspm/nodelibs-buffer@0.1.0": {
       "buffer": "npm:buffer@3.6.0"
@@ -877,11 +879,18 @@ System.config({
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:assert@1.3.0": {
+    "npm:assert@1.4.0": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "buffer-shims": "npm:buffer-shims@1.0.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
       "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.8.38": {
       "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:buffer-shims@1.0.0": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0"
     },
     "npm:buffer@3.6.0": {
       "base64-js": "npm:base64-js@0.0.8",
@@ -916,22 +925,14 @@ System.config({
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
-    "npm:lodash-compat@3.10.2": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:lodash-fp@0.10.4": {
-      "lodash-compat": "npm:lodash-compat@3.10.2"
-    },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:process@0.11.3": {
       "assert": "github:jspm/nodelibs-assert@0.1.0"
     },
-    "npm:rxjs@5.0.0-beta.7": {
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "symbol-observable": "npm:symbol-observable@0.2.4"
+    "npm:rx-lite@4.0.8": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:sinon@1.17.4": {
       "formatio": "npm:formatio@1.1.1",
